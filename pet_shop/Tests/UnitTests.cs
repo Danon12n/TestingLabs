d34877e912655563fb8d4dbad12a6c4db45010c7
whitestridee <88278878+whitestridee@users.noreply.github.com>
2022-12-17 16:37:42 +0300

    Add tests
    Add

--- pet_shop/Tests/UnitTests.cs
+++ pet_shop/Tests/UnitTests.cs
@@ -5,6 +5,11 @@ using NUnit.Framework;
 using pet_shop.Models;
 using System;
 using pet_shop.MySQLRepository;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using pet_shop.Controllers;
+
 namespace pet_shop.Tests
 {
     [AllureNUnit]
@@ -32,14 +37,43 @@ namespace pet_shop.Tests
         [AllureId(123)]
         public void EvenTest()
     {
+            //Arrange
             SayHello();
             PetMySQLRepository rep = new PetMySQLRepository();
+            //Act
             Pet newpet = rep.GetPetById(3);
             Console.WriteLine(newpet.availability);
-            //Wrapping Step
+            //Assert
             AllureLifecycle.Instance.WrapInStep(
                 () => { Assert.IsTrue(newpet.availability == "no", $"Oh no"); },
                 "Validate calculations");
         }
+
+        [Test]
+        [AllureTag("NUnit", "Debug")]
+        [AllureIssue("GitHub#1", "https://github.com/unickq/allure-nunit")]
+        [AllureSeverity(SeverityLevel.critical)]
+        [AllureFeature("Core")]
+        [AllureId(2)]
+        public void AddPetLondon()
+        {
+                //Arrange
+                SayHello();
+            var formCol = new FormCollection(new Dictionary<string, Microsoft.Extensions.Primitives.StringValues>
+                {
+                    { "shop_id", "1" },
+                    { "price", "200" },
+                    { "can_swim", "Yes" },
+                    {"reporoduce_ability", "Yes" }
+                });
+            var controllerMock = new Mock<VendorController>();
+            controllerMock.Setup(x => x.AddNewPet(formCol));
+            //Act
+            controllerMock.Object.AddNewPet(formCol);
+            //Assert
+            AllureLifecycle.Instance.WrapInStep(() => controllerMock.Verify(
+                x => x.RedirectToAction("Index", "Home"), Times.Once));
+        }
+
     }
 }
